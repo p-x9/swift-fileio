b104bdecdd5b309eebf2b16d6c1360436e12842e
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-03-23 08:21:34 +0900

    Fixed check for availability of `FileHandle.synchronize`

--- Sources/FileIO/StreamedFile.swift
+++ Sources/FileIO/StreamedFile.swift
@@ -61,7 +61,7 @@ extension StreamedFile {
     }
 
     public func sync() {
-        if #available(macOS 10.15, *) {
+        if #available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *) {
             try? fileHandle.synchronize()
         } else {
             fileHandle.synchronizeFile()
